fcaa43c74aa123fa2f91b94c5dae940108b9f453
wq <1963594288@qq.com>
2019-04-28 17:40:26 +0800

    Thread 1.0

--- src/BlockedJion01.java
+++ src/BlockedJion01.java
@@ -1,9 +1,18 @@
 
 public class BlockedJion01 {
-	public static void main(String[] args){
-		
+	public static void main(String[] args) throws InterruptedException{
+		Thread t = new Thread(()->{
+			for (int i = 0; i < 100; i++) {
+				System.out.println("lambda...."+i);
+			}
+		});
+		t.start();
+		for (int i = 0; i < 100; i++) {
+			if(i==20) {
+				t.join();;
+			}
+			System.out.println("main...."+i);
+		}
 	}
-	void test1() throws InterruptedException {
-		
-	}
+	
 }
